af0a9972d4fa73029a309c5cd86de469133551a8
MashkinA <cool.solt12@ya.ru>
2025-07-31 22:06:51 +0300

    init CreateBackgroundPage

---
+++ src/components/BackDescription/BackDescription.tsx
@@ -0,0 +1,64 @@
+import cl from "./BackDescription.module.css";
+import type { Background } from "../types.ts";
+
+export type BackDescriptionProps = {
+    selectedBack: Background;
+};
+
+const cornerImg = "/assets/icons/corner.webp";
+
+export const BackDescription = ({ selectedBack }: BackDescriptionProps) => {
+
+    return (
+        <div className={cl.background}>
+            <img className={cl.backgroundCorner1} src={ cornerImg } alt=""/>
+            <img className={cl.backgroundCorner2} src={ cornerImg } alt=""/>
+            <img className={cl.backgroundCorner3} src={ cornerImg } alt=""/>
+            <img className={cl.backgroundCorner4} src={ cornerImg } alt=""/>
+
+            <div className={cl.backgroundGrid}>
+                <div className={cl.backgroundGrid_left}>
+                    {/** Уровень описания подрассы */}
+                    <h2 className={cl.backgroundDescription}>{selectedBack.description}</h2>
+                    {/** Уровень описания способности */}
+                    <span className={cl.backgroundSkills}>
+                        <span className={cl.backgroundSkillsTitle}>Уникальный навык: </span>
+                        <span className={cl.backgroundDescription}>{selectedBack.description}</span>
+                    </span>
+                </div>
+
+                <div className={cl.backgroundGrid_right}>
+                    {/** Уровень описания навыков */}
+                    <span className={cl.backgroundSkills}>
+                        <span className={cl.backgroundSkillsTitle}>Владение навыками: </span>
+
+                        {selectedBack.attainments.map((char, index) => (
+                            <span className={cl.backgroundSkillsValue} key={char}>
+                                {char}{index === selectedBack.attainments.length - 1 ? '.' : ', '}
+                            </span>
+                        ))}
+                    </span>
+                    {/** Уровень описания инструментов */}
+                    <span className={cl.backgroundSkills}>
+                        <span className={cl.backgroundSkillsTitle}>Владение инструментами: </span>
+
+                        {selectedBack.instruments.map((char, index) => (
+                            <span className={cl.backgroundSkillsValue} key={char}>
+                                {char}{index === selectedBack.instruments.length - 1 ? '.' : ', '}
+                            </span>
+                        ))}
+                    </span>
+                    {/** Уровень описания снаряжения */}
+                    <span className={cl.backgroundSkills}>
+                        <span className={cl.backgroundSkillsTitle}>Снаряжения: </span>
+
+                        {selectedBack.equipment.map((char, index) => (
+                            <span className={cl.backgroundSkillsValue} key={char}>
+                                {char}{index === selectedBack.equipment.length - 1 ? '.' : ', '}
+                            </span>
+                        ))}
+                    </span>
+                </div>
+            </div>
+        </div>
+    )};
---
+++ src/components/BackSelector/BackSelector.tsx
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+import cl from './BackSelector.module.css';
+import type { Background } from "../types.ts";
+
+type BackSelectorProps = {
+    onCreate: (value: number) => void;
+    itemList: Background[];
+}
+const MIDDLE_ITEM = 2;
+
+export const BackSelector = ({ itemList, onCreate }: BackSelectorProps) => {
+
+    // Начальное значение элемента из списка расс
+    const [currentIndex, setCurrentIndex] = useState<number>(0);
+
+    // Определение количества отображаемых элементов на странице
+    const getSlideIndex = (): number[] => {
+        const len = itemList.length;
+        return [
+            (currentIndex - 2 + len) % len,
+            (currentIndex - 1 + len) % len,
+            currentIndex % len,
+            (currentIndex + 1) % len,
+            (currentIndex + 2) % len,
+        ];
+    };
+
+    // Функция перехода к следующему элементу из списка расс
+    const toPrevSlide = () => {
+        setCurrentIndex((prev) => (prev - 1 + itemList.length) % itemList.length);
+        onCreate(itemList[(currentIndex - 1 + itemList.length) % itemList.length].backgroundId);
+    };
+
+    // Функция перехода к предыдущему элементу из списка расс
+    const toNextSlide = () => {
+        setCurrentIndex((prev) => (prev + 1) % itemList.length);
+        onCreate(itemList[(currentIndex + 1) % itemList.length].backgroundId);
+    };
+
+    const handleSlideClick = (index: number) => {
+        setCurrentIndex(index);
+        onCreate(itemList[index].backgroundId);
+    };
+
+    // Количество отображаемых элементов
+    const visibleItems = getSlideIndex();
+
+    return (
+        <div className={cl.slider}>
+            <div className={cl.prevBtn} onClick={toPrevSlide} />
+
+            <div className={cl.slidesContainer}>
+                {visibleItems.map((item, i) => (
+
+                    <div
+                        className={i === MIDDLE_ITEM ? cl.slideActive : cl.slide}
+                        key={item}
+                        onClick={() => handleSlideClick(item)}>
+                        <img
+                            className={cl.itemImage}
+                            src={itemList[item].image}
+                            alt="Изображение выбранной предыстории"
+                        />
+
+                        <b className={cl.itemTitle}>{itemList[item].title}</b>
+
+                        <div className={i === MIDDLE_ITEM ? cl.slideBack : cl.slideNoBack}/>
+                    </div>
+
+                ))}
+            </div>
+
+            <div className={cl.nextBtn} onClick={toNextSlide}/>
+
+        </div>
+    );
+};
