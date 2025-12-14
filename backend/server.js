421e34c9036d7835cbc4e5b94b83e3365dc1ce5e
MashkinA <cool.solt12@ya.ru>
2025-12-14 20:41:43 +0300

    feat(deploy): фиксы для деплоя

--- backend/server.js
+++ backend/server.js
@@ -33,10 +33,12 @@ app.get('/', (req, res) => res.send('API is running'));
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, 'tressym/dist')));
+// Статические файлы фронтенда
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-app.all('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'tressym/dist', 'index.html'));
+// Catch-all для React SPA
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
 });
 
 app.use((err, req, res, next) => {
